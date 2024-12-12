32b7c4330ac8246ee652735edaad297d4a2eec45
Chethmina Kavishan <atadeka999@gmail.com>
2024-12-12 14:26:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "aFhmDDCI#RYhS71FCrnKSCeRbpqeAUUuPlMtuT5nU_5NV1x_DtgY"
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/8124133c3f031b0b5acb1.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm ChethminaMD. My owner is Chethmina Kavishan",
 };
