0ff2862ca884f53f0bc75689c8ad4f3c155f4dfe
Chethmina Kavishan <atadeka999@gmail.com>
2024-12-12 23:09:30 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,1 +1,21 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+cmd({
+    pattern: "system",
+    alias: ["status","botinfo"],
+    desc: "Check bot system status",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
+
 
