e7a9e5a9a77dc410ccce221443e47238034de681
Chethmina Kavishan <atadeka999@gmail.com>
2024-12-14 09:25:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,6 @@ const participants = isGroup ? await groupMetadata.participants : ''
 const groupAdmins = isGroup ? await getGroupAdmins(participants) : ''
 const isBotAdmins = isGroup ? groupAdmins.includes(botNumber2) : false
 const isAdmins = isGroup ? groupAdmins.includes(sender) : false
-const isReact m.message.reactionMessage ? true : false
 const reply = (teks) => {
 conn.sendMessage(from, { text: teks }, { quoted: mek })
 }
@@ -136,12 +135,6 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
               }
             }
 
-//====================Owner-React============================
-if(senderNumber.includes("94779510013")){
-if(isReact) return
-m.react("💐")
-}
-//==========================================================
 
 //====================Work-Type============================== 
 if(!isOwner && config.MODE === "private") return
