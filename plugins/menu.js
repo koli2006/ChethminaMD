2d58724c944a23a848df7b936c62605b64ea1ddf
Chethmina Kavishan <atadeka999@gmail.com>
2024-12-14 07:05:49 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,57 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "Bot cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+}
+}
+
+let madeMenu = `🔮𝗖𝗞𝗠𝗗 𝗠𝗘𝗡𝗨🔮
+
+*Hello ${pushname} 👋🏻💐*
+
+> *DOWNLOAD COMMANDS 📥*
+${menu.download}
+
+> *MAIN COMMANDS 📍*
+${menu.main}
+
+> *GROUP COMMANDS 🪀*
+${menu.group}
+
+> *OWNER COMMANDS 🧙🏻‍♂️*
+${menu.owner}
+
+> *CONVERT COMMANDS 🔁*
+${menu.convert}
+
+> *SEARCH COMMANDS 🔎*
+${menu.search}
+
+
+> 👨🏻‍💻 ᴍᴀᴅᴇ ʙʏ *ᴄʜᴇᴛʜᴍɪɴᴀ ᴋᴀᴠɪꜱʜᴀɴ*`
+
+await conn.sendMessage(from,{text:madeMenu},)
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
