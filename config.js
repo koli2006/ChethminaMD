ea55d04a8016ec7eade1d019c55ff686941f59d9
Chethmina Kavishan <atadeka999@gmail.com>
2024-12-19 10:32:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "aFhmDDCI#RYhS71FCrnKSCeRbpqeAUUuPlMtuT5nU_5NV1x_DtgY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://envs.sh/8HL.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello ${pushname} 👋🏻💐. I am Alive Now⚡*\n\n⏰ Now time is @time\n📅 Date : @date\n\n🟢 I am online still ${runtime(process.uptime())}\n💻 Host : ${os.hostname()}\n🪢 Version : 1.0.0\n\n*Type .menu to get full commands list.*\n\n\n> 👨🏻‍💻 ᴍᴀᴅᴇ ʙʏ *ᴄʜᴇᴛʜᴍɪɴᴀ ᴋᴀᴠɪꜱʜᴀɴ*",
+ALIVE_MSG: process.env.ALIVE_MSG || "◅          🧙🏻‍♂️𝗖𝗛𝗘𝗧𝗛𝗠𝗜𝗡𝗔 𝗠𝗗🧙🏻‍♂️          ▻\n\n*Hey there I'm alive now! 💐*\n\nʜᴇʀᴇ'ꜱ ᴡʜᴀᴛ ᴄᴀɴ ɪ ᴅᴏ👇🏻\n* *ᴅᴏᴡɴʟᴏᴀᴅ ꜱᴏɴɢ & ᴠɪᴅᴇᴏ📥*\n* *ꜰᴇᴛᴄʜ ʟᴀᴛᴇꜱᴛ ɴᴇᴡꜱ📰*\n* *ᴍᴀɴᴀɢᴇ ɢʀᴏᴜᴘ👮🏻‍♂️*\n* *ᴅᴏᴡɴʟᴏᴀᴅ ɢᴏᴏɢʟᴇ ᴅʀɪᴠᴇ, ᴍᴇᴀᴅɪᴀꜰɪʀᴇ ꜰɪʟᴇꜱ🗂️*\n\n*Type _.menu_ to get full commands list 📃*\n\n\n> 👨🏻‍💻 ᴍᴀᴅᴇ ʙʏ *ᴄʜᴇᴛʜᴍɪɴᴀ ᴋᴀᴠɪꜱʜᴀɴ*",
 MODE: process.env.MODE || "public",
 };
