5c9b1762f09ac4a8adc3fd69ebe740d4e15ac647
Chethmina Kavishan <atadeka999@gmail.com>
2024-12-19 09:51:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "aFhmDDCI#RYhS71FCrnKSCeRbpqeAUUuPlMtuT5nU_5NV1x_DtgY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/8124133c3f031b0b5acb1.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello I'm ChethminaMD. My owner is Chethmina Kavishan",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello ${pushname} 👋🏻💐. I am Alive Now⚡*\n\n⏰ Now time is @time\n📅 Date : @date\n\n🟢 I am online still ${runtime(process.uptime())}\n💻 Host : ${os.hostname()}\n🪢 Version : 1.0.0\n\n*Type .menu to get full commands list.*\n\n\n> 👨🏻‍💻 ᴍᴀᴅᴇ ʙʏ *ᴄʜᴇᴛʜᴍɪɴᴀ ᴋᴀᴠɪꜱʜᴀɴ*",
 MODE: process.env.MODE || "public",
 };
